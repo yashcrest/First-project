f14deb066491fd52cb65a99a074a511b926ba9ba
yashcrest <yashcrest@gmail.com>
2022-02-22 11:04:18 +1100

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,6 +1,19 @@
-(function(){
+document.getElementById("btn1").onclick = function(){
+    document.getElementById("confirm").innerHTML = "Order Placed. Check email for confirmation";
+    document.getElementById("btn1").style.display= "none";
+    document.getElementById("btn2").style.display = "block";
+    console.log("log");
+}
+document.getElementById("btn2").onclick = function(){
+    document.getElementById("btn1").style.display = "block";
+    document.getElementById("btn2").style.display = "none";
+    document.getElementById("confirm").innerHTML = "confirm purchase?";
+    console.log("log");
+}
 
-    var age = 5;  
-})();
+let myname = "Yash";
+myname = myname.toUpperCase();
+console.log(myname);
 
-var y = 5; 
+
+
